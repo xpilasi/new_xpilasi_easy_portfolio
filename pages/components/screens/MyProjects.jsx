0e3010ae5d8fa24fbd93df43bc46e8e33071584b
xpilasi <pilasiha@gmail.com>
2024-01-21 22:59:26 +0100

    web fodlers

--- pages/components/screens/MyProjects.jsx
+++ pages/components/screens/MyProjects.jsx
@@ -6,7 +6,8 @@ import web3 from '../../../public/img/web3.png'
 import web4 from '../../..//public/img/web4.png'
 import web5 from '../../../public/img/just_list_web.png'
 import web6 from '../../../public/img/gorilla_web.png'
-import justList from '../../../public/img/just_list_web_full.png'
+import gorillaWeb from '../../../public/img/final_webs/gorilla_web2.png'
+import justList from '../../../public/img/final_webs/just_list_web_full.png'
 
 import Title from '../widgets/Title';
 import MiniSubTitle from '../widgets/MiniSubTitle';
@@ -27,7 +28,7 @@ const MyProjects = () => {
         <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
           {/* project 1 */}
           <div className=' basis-1/3 flex-1'>
-            <Image src={web6} className=' rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'></Image>
+            <Image src={gorillaWeb} className=' rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'></Image>
           </div>
           {/* project 2 */}
           <div className=' basis-1/3 flex-1'>
