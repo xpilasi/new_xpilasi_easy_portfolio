f2bbcf05efc6ea75234ab06f3c646ed8914e7401
Xavier Pilasi <pilasiha@gmail.com>
2024-07-28 17:46:11 +0200

    bigger title and subtitles

--- pages/components/widgets/MiniSubTitle.jsx
+++ pages/components/widgets/MiniSubTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MiniSubTitle = ({miniSubTitle}) => {
 
-    const miniSubTitleStyle = ' text-md md:text-xl font-light py-1  text-neon-grey';
+    const miniSubTitleStyle = ' text-xl md:text-xl font-light py-1  text-neon-grey';
     
   return (
 
--- pages/components/widgets/SubTitle.jsx
+++ pages/components/widgets/SubTitle.jsx
@@ -4,7 +4,7 @@ import Image  from 'next/image';
 const SubTitle = ({srcSubtitle}) => {
 
     const subTitleStyle = 
-                  `text-3xl lg:text-4xl md:text-4xl font-bold py-1 font-exo  
+                  `text-4xl lg:text-4xl md:text-4xl font-bold py-1 font-exo  
                    text-neon-water mt-10 text-left pl-10 bg-neon-blueberry leading-tight`;
     
   return (
