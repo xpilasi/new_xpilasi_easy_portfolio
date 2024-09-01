12b181054e9e6021850c3a2fdfe3563ef5c98b72
Xavier Pilasi <pilasiha@gmail.com>
2024-09-01 14:28:47 +0200

    test

--- pages/components/screens/sublevel/Features.jsx
+++ pages/components/screens/sublevel/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image  from 'next/image';
+import Column from '../../widgets/columns/Column';
 
 const Features = ({year,framework,language,backend}) => {
 
@@ -7,22 +8,22 @@ const Features = ({year,framework,language,backend}) => {
     
   return (
 
-    <div className='grid grid-cols-4 py-5 lg:py-0 text-xs lg:text-sm'>
-      <div className='text-zinc-500 font-interDisplayLight '>Year</div>
+    <div className='grid grid-cols-4 py-5 lg:py-0 text-xs lg:text-sm w-full justify-between'>
       
+      <Column header='Year' content={year}/>
+      <Column header='Framework' content={framework}/>
+      <Column header='Language' content={language}/>
+      <Column header='Backend' content={backend}/>
       
+      {/* <div className='text-zinc-500 font-interDisplayLight '>Year</div>
       <div className='text-zinc-500 font-interDisplayLight '>Framework</div>
-      
       <div className='text-zinc-500 font-interDisplayLight '>Language</div>
       <div className='text-zinc-500 font-interDisplayLight '>Backend</div>
-      
-      
-      
       
       <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{year}</div>
       <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{framework}</div>
       <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{language}</div>
-      <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{backend}</div>
+      <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{backend}</div> */}
       
     </div>
    
--- pages/components/widgets/columns/Column.jsx
+++ pages/components/widgets/columns/Column.jsx
@@ -4,11 +4,11 @@ const Column = (header,content) => {
   return (
     <div className='flex flex-col'>
         
-        <div className=''>{/* HEADER */}
+        <div className='text-zinc-500 font-interDisplayLight'>{/* HEADER */}
             {header}
         </div>
 
-        <div className=''>{/* CONTENT */}
+        <div className='text-white font-interdisplayLight  pb-12 tracking-wider'>{/* CONTENT */}
             {content}
         </div>
 
