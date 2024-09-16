7dc3540ec0d22e77bb30e0aadaf8707b185b0c85
Xavier Pilasi <pilasiha@gmail.com>
2024-09-16 23:09:27 +0200

    mobile settings

--- pages/components/screens/sublevel/Roadmap.jsx
+++ pages/components/screens/sublevel/Roadmap.jsx
@@ -26,19 +26,22 @@ const Roadmap = ({showRoadMap, setShowRoadMap}) => {
       );
     const yearDesc2 = (
         <>
-          After gaining several years of experience in <span className='text-neon-green'>Team leadership</span>  negotiation, and project launches, I assumed the role of <span className='text-neon-green'>Business Development Manager</span> at a renowned company in Latin America. In this position, I represented important  Brands and launched new <span className='text-neon-green'>technology projects</span>. 
+          After gaining several years of experience in <span className='text-neon-green'>Team leadership</span>,  negotiation, and project launches, I assumed the role of <span className='text-neon-green'>Business Development Manager</span> at a renowned company in Latin America. 
+          {/* In this position, I represented important  Brands and launched new <span className='text-neon-green'>technology projects</span>.  */}
    
     
         </>
       );
     const yearDesc3 = (
         <>
-          I decide to embark on the search for new professional paths and choose <span className='text-neon-green'>Barcelona</span> as my launching pad. I discover the world of <span className='text-neon-green'>programming</span> through various contacts and am captivated. I begin my first course at the IT Academy of Barcelona, where I learn the fundamentals of programming, then I dive into <span className='text-neon-green'>Python</span>, <span className='text-neon-green'>SQL</span> and <span className='text-neon-green'>Django</span>.
+          {/* I decide to embark on the search for new professional paths and choose <span className='text-neon-green'>Barcelona</span> as my launching pad.  */}
+          I discover the world of <span className='text-neon-green'>programming</span> through various contacts and am captivated. I begin my first course at the IT Academy of Barcelona, where I learn the fundamentals of programming, then I dive into <span className='text-neon-green'>Python</span>, <span className='text-neon-green'>SQL</span> and <span className='text-neon-green'>Django</span>.
         </>
       );
     const yearDesc4 = (
         <>
-          I was very curious about how could I build easier solutions for the day to day so I give a shot to the <span className='text-neon-green'>mobile development</span> with <span className='text-neon-green'>Flutter</span>. I focused on my personal backlog requirements to build Android and IOs apps with <span className='text-neon-green'>Dart</span>. Most of my skills acquired in the past were super useful: I managed to launch one Beta in <span className='text-neon-green'>Google Play</span> and build a complex <span className='text-neon-green'>FullStack climbing app</span>.
+          {/* I was very curious about how could I build easier solutions for the day to day so I give a shot to the <span className='text-neon-green'>mobile development</span> with <span className='text-neon-green'>Flutter</span>.  */}
+          I focused on my personal backlog requirements to build Android and IOs apps with <span className='text-neon-green'>Flutter</span>. Most of my skills acquired in the past were super useful: I managed to launch one Beta in <span className='text-neon-green'>Google Play</span> and build a complex <span className='text-neon-green'>FullStack climbing app</span>.
         </>
       );
     const yearDesc5 = (
--- pages/components/widgets/titles/RoadmapTitle.jsx
+++ pages/components/widgets/titles/RoadmapTitle.jsx
@@ -6,7 +6,7 @@ const RoadmapTitle = ({titleText}) => {
     
   return (
      
-    <h2 className='text-white font-interDisplayMedium lg:text-[80px] text-[40px]'>
+    <h2 className='text-white font-interDisplayMedium lg:text-[50px] xl:text[80px] text-[40px]'>
         {titleText}
     </h2>
     
