d4fce7d2345ec221cd3f2de63c550ce93215327c
xpilasi <pilasiha@gmail.com>
2025-03-02 15:30:23 +0100

    hubspot tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import '@/styles/globals.css'
 import '@splidejs/react-splide/css';
 import '@/styles/splide-custom.css';
+import Script from 'next/script';
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    {/* Código de seguimiento de HubSpot */}
+    <Script
+          id="hs-script-loader" // Mantiene el ID original de HubSpot
+          strategy="afterInteractive"
+          src="//js.hs-scripts.com/49442398.js"
+          async
+          defer
+        />
+        
+    <Component {...pageProps} />
+  </>
+  );
 }
